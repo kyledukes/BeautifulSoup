eb6dbfcb0426d52ba6aa2911eaaee3b72f68f81b
kyledukes <dukeskyler@gmail.com>
2016-11-15 13:14:09 -0800

    Add files via upload
    Scraping Oscar De La Hoya's opponents from his wikipedia page.

---
+++ oscarDeLaHoya.py
@@ -0,0 +1,31 @@
+from bs4 import BeautifulSoup
+import urllib2
+
+file = open('oscar.txt', 'w+')
+errorFile = open('errorOscar.txt', 'w+')
+
+url = urllib2.urlopen("https://en.wikipedia.org/wiki/Oscar_De_La_Hoya")
+soup = BeautifulSoup(url, "lxml")
+
+x = 0
+
+for i in soup.find_all('table', class_="wikitable")[1]:
+	for row in i.find_all('tr'):
+		y = 0
+		for name in row.find_all('td', style="text-align:left;")[0:1]:
+			x = x + 1
+			try:
+				file.write(name.text+' ')
+			except UnicodeEncodeError as e:
+				errorFile.write(str(x)+' '+str(e)+'\n')
+				y = y + 1
+				pass
+		for result in row.find_all('td')[1:2]:
+				if y == 0:
+					try:
+						file.write(result.text+'\n')
+					except UnicodeEncodeError as e:
+						errorFile.write(str(x)+' '+str(e)+'\n')
+						pass
+file.close()
+errorFile.close()
