76796d228713d50fbc5c10e52f51928b7febacb5
kyledukes <dukeskyler@gmail.com>
2016-11-15 20:31:17 -0800

    Update oscarDeLaHoya.py

--- oscarDeLaHoya.py
+++ oscarDeLaHoya.py
@@ -7,25 +7,25 @@ errorFile = open('errorOscar.txt', 'w+')
 url = urllib2.urlopen("https://en.wikipedia.org/wiki/Oscar_De_La_Hoya")
 soup = BeautifulSoup(url, "lxml")
 
-x = 0
+table = soup.find_all('table', class_="wikitable")[1]
 
-for i in soup.find_all('table', class_="wikitable")[1:2]:
-	for row in i.find_all('tr'):
-		y = 0
-		for name in row.find_all('td', style="text-align:left;")[0:1]:
-			x = x + 1
+x = 0
+for row in table.find_all('tr'):
+	y = 0
+	for name in row.find_all('td', style="text-align:left;")[0:1]:
+		x += 1
+		try:
+			file.write(name.text+' ')
+		except Exception as e:
+			errorFile.write(str(x)+' '+str(e)+'\n')
+			y += 1
+			pass
+	for result in row.find_all('td')[1:2]:
+		if y == 0:
 			try:
-				file.write(name.text+' ')
-			except UnicodeEncodeError as e:
+				file.write(result.text+'\n')
+			except Exception as e:
 				errorFile.write(str(x)+' '+str(e)+'\n')
-				y = y + 1
 				pass
-		for result in row.find_all('td')[1:2]:
-			if y == 0:
-				try:
-					file.write(result.text+'\n')
-				except UnicodeEncodeError as e:
-					errorFile.write(str(x)+' '+str(e)+'\n')
-					pass
 file.close()
 errorFile.close()
