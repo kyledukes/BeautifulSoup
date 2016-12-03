42eabd32d72f1f9539f0c36198f37387ed1134ea
kyledukes <dukeskyler@gmail.com>
2016-12-02 19:34:25 -0800

    Update oscarDeLaHoya.py

--- oscarDeLaHoya.py
+++ oscarDeLaHoya.py
@@ -11,21 +11,21 @@ table = soup.find_all('table', class_="wikitable")[1]
 
 x = 0
 for row in table.find_all('tr'):
-	y = 0
-	for name in row.find_all('td', style="text-align:left;")[0:1]:
-		x += 1
-		try:
-			file.write(name.text+' ')
-		except Exception as e:
-			errorFile.write(str(x)+' '+str(e)+'\n')
-			y += 1
-			pass
-	for result in row.find_all('td')[1:2]:
-		if y == 0:
-			try:
-				file.write(result.text+'\n')
-			except Exception as e:
-				errorFile.write(str(x)+' '+str(e)+'\n')
-				pass
+    y = 0
+    for name in row.find_all('td', style="text-align:left;")[0:1]:
+	x += 1
+	try:
+		file.write(name.text+' ')
+	except Exception as e:
+		errorFile.write(str(x)+' '+str(e)+'\n')
+		y += 1
+		pass
+    for result in row.find_all('td')[1:2]:
+        if y == 0:
+	    try:
+		file.write(result.text+'\n')
+	    except Exception as e:
+	        errorFile.write(str(x)+' '+str(e)+'\n')
+	        pass
 file.close()
 errorFile.close()
